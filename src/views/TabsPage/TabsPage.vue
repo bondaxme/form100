a4e0c6cf2536775a36f7f9893ef2c47f0f998db8
bondaxme <maksbond280104@gmail.com>
2025-04-18 02:49:53 +0300

    add switch tab styles

--- src/views/TabsPage/TabsPage.vue
+++ src/views/TabsPage/TabsPage.vue
@@ -16,17 +16,17 @@
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" @click="onGoTo('/tabs/tab1')">
+        <ion-tab-button tab="tab1" @click="onGoTo('/tabs/tab1')" :class="{ active: isActive('tab1') }">
           <ion-icon aria-hidden="true" :icon="listOutline" />
           <ion-label>Репорти</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab2" @click="onGoTo('/tabs/tab2')">
+        <ion-tab-button tab="tab2" @click="onGoTo('/tabs/tab2')" :class="{ active: isActive('tab2') }">
           <ion-icon aria-hidden="true" :icon="createOutline" />
           <ion-label>Новий</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab3" @click="onGoTo('/tabs/tab3')">
+        <ion-tab-button tab="tab3" @click="onGoTo('/tabs/tab3')" :class="{ active: isActive('tab3') }">
           <ion-icon aria-hidden="true" :icon="settingsOutline" />
           <ion-label>Налаштування</ion-label>
         </ion-tab-button>
@@ -53,13 +53,18 @@ export default defineComponent({
 
     const title = ref('Список звітів');
     const route = useRoute();
+    
+    const isActive = (tab: string) => {
+      return route.path.includes(`/tabs/${tab}`);
+    };
+
     watch(() => route.path, (newPath) => {
       if (newPath.includes('/tabs/tab1')) {
         title.value = 'Список звітів';
       } else if (newPath.includes('/tabs/tab2')) {
         title.value = 'Новий звіт';
       } else if (newPath.includes('/tabs/tab3')) {
-        title.value = 'Завантажити дані';
+        title.value = 'Налаштування';
       } else if (newPath.includes('/tabs/report')) {
         title.value = 'Деталі звіту';
       } else if (newPath.includes('/tabs/staff')) {
@@ -70,6 +75,7 @@ export default defineComponent({
       onGoTo,
       goBack,
       title,
+      isActive,
       createOutline,
       settingsOutline,
       listOutline,
