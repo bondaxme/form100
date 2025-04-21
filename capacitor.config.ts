e8f77dc161ba8e420f2c10b4789d45c09005a875
bondaxme <maksbond280104@gmail.com>
2025-04-21 05:14:38 +0300

    add pwa build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.reportmobileapp.app',
-  appName: 'Report Mobile App',
+  appName: 'Report Mobile App PWA',
   webDir: 'dist'
 };
 
